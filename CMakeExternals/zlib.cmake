2b91bf602aff07df30ba150aadbd72d4a3adaac4
Andrey Fedorov <fedorov@bwh.harvard.edu>
2016-02-25 11:18:03 -0500

    ENH: tinkering with zlib

--- CMakeExternals/zlib.cmake
+++ CMakeExternals/zlib.cmake
@@ -4,8 +4,12 @@ set(proj zlib)
 # Set dependency list
 set(${proj}_DEPENDENCIES "")
 
+message("===> Building zlib!")
+
 # Include dependent projects if any
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+message("===> Building zlib after dependencies!")
 
 if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   message("===> Using system zlib!")
@@ -21,7 +25,7 @@ if(DEFINED zlib_DIR AND NOT EXISTS ${zlib_DIR})
 endif()
 
 if(NOT DEFINED zlib_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
-  message("HERE")
+  message("zlib HERE")
   if(NOT DEFINED git_protocol)
     set(git_protocol "git")
   endif()
