6baf0b99221dd8222d80e95e50b913d6ae4db0a8
Christian Herz <christianherz1985@gmail.com>
2016-05-23 11:32:24 -0400

    ENH: Moved cli code from segimage2itkimage to library - fixed unresolved symbols

--- apps/seg/ImageSEGConverter.h
+++ apps/seg/ImageSEGConverter.h
@@ -1,6 +1,10 @@
 #ifndef DCMQI_CONVERTER_H
 #define DCMQI_CONVERTER_H
 
+#include "dcmtk/config/osconfig.h"   // make sure OS specific configuration is included first
+
+#include "vnl/vnl_cross.h"
+
 #ifdef WITH_ZLIB
 #include <zlib.h>                     /* for zlibVersion() */
 #endif
@@ -16,6 +20,14 @@
 #include "dcmtk/dcmfg/fgplanor.h"
 #include "dcmtk/dcmfg/fgpixmsr.h"
 #include "dcmtk/dcmfg/fgplanpo.h"
+#include "dcmtk/dcmfg/fgseg.h"
+
+#include "dcmtk/dcmseg/segutils.h"
+
+
+#include "dcmtk/oflog/loglevel.h"
+
+#include "dcmtk/dcmdata/dcrledrg.h"
 
 // UIDs
 #include "QIICRUIDs.h"
@@ -26,12 +38,13 @@
 //#include "preproc.h"
 
 #include <itkImageFileReader.h>
-#include <itkLabelImageToLabelMapFilter.h>
+#include <itkImageFileWriter.h>
+#include <itkImageDuplicator.h>
 #include <itkImageRegionConstIterator.h>
+#include <itkLabelImageToLabelMapFilter.h>
 #include <itkLabelStatisticsImageFilter.h>
 #include <itkBinaryThresholdImageFilter.h>
-
-//#include "Helper.h"
+#include <itkChangeInformationImageFilter.h>
 
 #include "JSONMetaInformationHandler.h"
 
@@ -44,6 +57,8 @@ typedef itk::Image<PixelType, 3> ImageType;
 typedef itk::ImageFileReader<ImageType> ReaderType;
 typedef itk::LabelImageToLabelMapFilter<ImageType> LabelToLabelMapFilterType;
 
+static OFLogger dcemfinfLogger = OFLog::getLogger("qiicr.apps");
+
 namespace dcmqi {
 
     class ImageSEGConverter {
@@ -52,7 +67,7 @@ namespace dcmqi {
         static bool itkimage2dcmSegmentation(vector<string> dicomImageFileNames, vector<string> segmentationFileNames,
                                              const char *metaDataFileName, const char *outputFileName);
 
-        static bool dcmSegmentation2itkimage();
+        static bool dcmSegmentation2itkimage(const char *inputSEGFileName, const char *outputDirName);
         static int CHECK_COND(const OFCondition& condition);
 
     private:
@@ -60,6 +75,11 @@ namespace dcmqi {
         static ContentIdentificationMacro createContentIdentificationInformation();
         static vector<int> getSliceMapForSegmentation2DerivationImage(const vector<string> &dicomImageFileNames,
                                                                       const itk::Image<short, 3>::Pointer &labelImage);
+
+        static int getImageDirections(FGInterface &fgInterface, ImageType::DirectionType &dir);
+        static int getDeclaredImageSpacing(FGInterface &fgInterface, ImageType::SpacingType &spacing);
+        static int computeVolumeExtent(FGInterface &fgInterface, vnl_vector<double> &sliceDirection,
+                                       ImageType::PointType &imageOrigin, double &sliceSpacing, double &sliceExtent);
     };
 
 }
