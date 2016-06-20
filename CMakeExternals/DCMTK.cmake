594eb82cea75a1a15acc156c8668bfedabb022d2
Andrey Fedorov <fedorov@bwh.harvard.edu>
2016-06-20 15:46:45 +0200

    COMP: update gith hash

--- CMakeExternals/DCMTK.cmake
+++ CMakeExternals/DCMTK.cmake
@@ -25,7 +25,7 @@ if(DEFINED DCMTK_DIR AND NOT EXISTS ${DCMTK_DIR})
 endif()
 
 if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
-  set(revision_tag DCMTK-3.6.1_20160216)
+  set(revision_tag d7deba3b3c98fd0d3c354da3d99c9cda5beb7833)
   if(${proj}_REVISION_TAG)
     set(revision_tag ${${proj}_REVISION_TAG})
   endif()
