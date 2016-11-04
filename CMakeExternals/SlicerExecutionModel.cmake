61d8a722c955aa8c8878a6b51d0badb88f9d09e9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-11-04 14:08:25 -0400

    ENH: Update SlicerExecutionModel
    $ git shortlog 311eff9..62d0121 --no-merges
    Francois Budin (1):
          BUG: Wrong unparsed arguments variable name
    
    Hans Johnson (6):
          COMP: KWSYS removed support for pre-C++98 in 2015
          ENH: Exceptions should be caught by reference
          STYLE: Prefere initialization to assignment
          STYLE: Remove unused function WriteLines
          STYLE: Remove unused variable 'elements'
          BUG: find_package(ITK ...) clobbered existing
    
    Jean-Christophe Fillion-Robin (19):
          STYLE: ModuleDescription: Move definition to cxx file
          STYLE: Remove obsolete ModuleFactory class
          STYLE: ModuleDescription: Remove unused Alternative Type/Target/Location
          STYLE: Remove obsolete BinaryFileDescriptor support
          STYLE: ModuleDescriptionParser: Add separator comment, small tweaks
          STYLE: Add ModuleDescriptionTesting Macros and Utilities files
          STYLE: ModuleDescription: Add TestDefaults
          ENH: ModuleDescription: Add support for lazily setting the target
          ENH: Update SEMMacroBuildCLI to copy (and optionally install) the XML file
          STYLE: CMakeLists: Consistently requires version 2.8.6
          STYLE: CMakeLists: Simplify code removing unneeded setting of CMP0016 to NEW
          STYLE: CMakeLists: Simplify code removing unneeded setting of CMP0017
          STYLE: CMakeLists: Simplify buildsystem using FindThreads module
          STYLE: CMakeLists: Remove obsolete option ModuleDescriptionParser_USE_PYTHON
          STYLE: CMakeLists: Finding of threading library is not needed anymore
          JsonSerializationUtilities: Remove unused method
          gitignore: Add CMakeLists.txt.user
          docker: Associate build-time metadata with the image
          README: Add docker badges
    
    Johan Andruejol (13):
          ENH: Improve JsonCpp support for SlicerExecutionModel
          ENH: Add JsonCpp path to the GenerateCLP launcher
          ENH: Do not pass the JsonCpp_DIR to the testing directly
          ENH: Allow CLI to deserialize without the required arguments
          ENH: Improve support for empty arrays
          ENH: ParameterSerializer: Use the longflag in priority
          ENH: ParameterSerializer: Fix multiple parameters handling for deserialization
          STYLE: GenerateCLP: Factorize out the CMakeLists of the testing CLIs
          ENH: ParameterSerializer: Improve escaping of the JSONModuleDescription
          ENH: GenerateCLP: Force GenerateCLPLauncher rebuild on GenerateCLP change
          ENH: Uniformize the use of JsonCpp with VTK
          BUG: Fix GenerateCLP_FORWARD_PATHS_[BUILD/INSTALL] variable
          ENH: GenerateCLP: Include jsoncpp include dirs in UseGenerateCLP.cmake.in
    
    Lucas Gandel (2):
          BUG: Fix ctest error on ITKExpat linking in GenerateCLP testing
          BUG: Fix link error related to ITKExpat when using ITK > v4.8.1
    
    Matt McCormick (3):
          ENH: Use hidden visibility on the CLI library.
          Revert "BUG: Fix ctest error on ITKExpat linking in GenerateCLP testing"
          COMP: Do not use hidden visibility when ITK is built statically.
    
    Stephen Aylward (1):
          WARN: Rename isPresent variables to avoid hidden local variable
    
    ivmartel (2):
          Made implicit conversion explicit (size_t to int).
          Using static_cast instead of direct cast.

--- CMakeExternals/SlicerExecutionModel.cmake
+++ CMakeExternals/SlicerExecutionModel.cmake
@@ -33,7 +33,7 @@ if(NOT DEFINED SlicerExecutionModel_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/Slicer/SlicerExecutionModel.git"
-    GIT_TAG "311eff9038be165c1341dab90196fbd7466e715a"
+    GIT_TAG "62d0121dbb0fb057ebbd7c9ab84520accacec8bc"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
