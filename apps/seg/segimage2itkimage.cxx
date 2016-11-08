1661afa61bb5caaf0326f96abd94dead6ae4cd5e
paul wighton <paul@corticometrics.com>
2016-11-08 12:59:15 -0500

    ENH: segimage2itkimage can now output niftis (--nii flag)

--- apps/seg/segimage2itkimage.cxx
+++ apps/seg/segimage2itkimage.cxx
@@ -20,7 +20,12 @@ int main(int argc, char *argv[])
   for(map<unsigned,ImageType::Pointer>::const_iterator sI=result.first.begin();sI!=result.first.end();++sI){
     typedef itk::ImageFileWriter<ImageType> WriterType;
     stringstream imageFileNameSStream;
-    imageFileNameSStream << outputDirName << "/" << outputPrefix << sI->first << ".nrrd";
+
+    if (niftiOutput) {
+      imageFileNameSStream << outputDirName << "/" << outputPrefix << sI->first << ".nii.gz";
+    } else {
+      imageFileNameSStream << outputDirName << "/" << outputPrefix << sI->first << ".nrrd";
+    }
 
     WriterType::Pointer writer = WriterType::New();
     writer->SetFileName(imageFileNameSStream.str().c_str());
