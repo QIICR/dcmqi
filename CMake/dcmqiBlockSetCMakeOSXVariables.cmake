09d03e24a83462400501991e4857f22b89d118cd
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-11-11 00:14:01 -0500

    cmake: Support building DCMQI as a Slicer extension
    * Detect case where the project should be built as a slicer extension
     * when built by the Slicer Extensions build system
     * when Slicer_DIR is passed as a configure option
    
    * Detect when built-in JsonCpp should be used
    
    * Update dcmqiBlockSetCMakeOSXVariables so that its explicitly
      asks the user to set CMAKE_OSX_DEPLOYMENT_TARGET after checking
      which runtime is associated with Qt.

--- CMake/dcmqiBlockSetCMakeOSXVariables.cmake
+++ CMake/dcmqiBlockSetCMakeOSXVariables.cmake
@@ -27,7 +27,8 @@
 #
 
 # Note: Change architecture *before* any enable_language() or project()
-#       calls so that it's set properly to detect 64-bit-ness...
+#       calls so that it's set properly to detect 64-bit-ness, and
+#       deployment target for the standard c++ library.
 #
 if(APPLE)
 
@@ -84,15 +85,42 @@ if(APPLE)
           string(REPLACE "." "" sdk_version_no_dot ${SDK_VERSION})
           set(OSX_NAME ${OSX_SDK_${sdk_version_no_dot}_NAME})
           set(CMAKE_OSX_ARCHITECTURES "x86_64" CACHE STRING "Force build for 64-bit ${OSX_NAME}." FORCE)
-          set(CMAKE_OSX_DEPLOYMENT_TARGET "${SDK_VERSION}" CACHE STRING "Force build for 64-bit ${OSX_NAME}." FORCE)
+          if(NOT Slicer_DIR)
+            set(CMAKE_OSX_DEPLOYMENT_TARGET "${SDK_VERSION}" CACHE STRING "Force build for 64-bit ${OSX_NAME}." FORCE)
+          endif()
           set(CMAKE_OSX_SYSROOT "${SDK_SYSROOT_NEWEST}" CACHE PATH "Force build for 64-bit ${OSX_NAME}." FORCE)
           message(STATUS "Setting OSX_ARCHITECTURES to '${CMAKE_OSX_ARCHITECTURES}' as none was specified.")
-          message(STATUS "Setting OSX_DEPLOYMENT_TARGET to '${CMAKE_OSX_DEPLOYMENT_TARGET}' as none was specified.")
+          if(NOT Slicer_DIR)
+            message(STATUS "Setting OSX_DEPLOYMENT_TARGET to '${CMAKE_OSX_DEPLOYMENT_TARGET}' as none was specified.")
+          endif()
           message(STATUS "Setting OSX_SYSROOT to latest '${CMAKE_OSX_SYSROOT}' as none was specified.")
         endif()
       endif()
     endif()
   endforeach()
+
+  if("x${CMAKE_OSX_DEPLOYMENT_TARGET}x" STREQUAL "xx" AND Slicer_DIR)
+    string(REGEX MATCH "MacOSX([0-9]+\\.[0-9]+)\\.sdk" _match "${CMAKE_OSX_SYSROOT}")
+    set(SDK_VERSION "${CMAKE_MATCH_1}")
+    if( "${SDK_VERSION}" VERSION_GREATER "10.8" )
+      # add to cache to allow interactive editing after fatal error
+      set(CMAKE_OSX_DEPLOYMENT_TARGET "" CACHE PATH "Deployment target needs to be explicitly set." FORCE)
+      message(FATAL_ERROR
+        "The OSX_SYSROOT is set to version ${SDK_VERSION} (>10.8) and OSX_DEPLOYMENT_TARGET is not explicitly set!\n"
+        "Since:\n"
+        " (1) the default runtime associated with >=10.9 deployment target is 'libc++'.[1]\n"
+        " (2) the default runtime associated with <=10.8 deployment target is 'libstdc++'.\n"
+        " (3) Qt support for 'macx-clang-libc++' is listed as 'unsupported' mkspecs.\n"
+        " (4) Qt binaries may be build against 'libstdc++' or 'libc++'.\n"
+        " (5) Mixing the two different runtime in binaries is unstable.\n"
+        "  [1]http://stackoverflow.com/questions/19637164/c-linking-error-after-upgrading-to-mac-os-x-10-9-xcode-5-0-1/19637199#19637199\n"
+        "--------------------------------\n"
+        "Run '$otool -L $(which qmake) |grep lib.\\*c++' to check what library Qt is built against:\n"
+        " (1) if it is libstdc++ then add '-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.8' (or older) to the cmake command line.\n"
+        " (2) if it is libc++ then add '-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9' (or newer) to the cmake command line.\n"
+        )
+    endif()
+  endif()
 
   if(NOT "${CMAKE_OSX_SYSROOT}" STREQUAL "")
     if(NOT EXISTS "${CMAKE_OSX_SYSROOT}")
