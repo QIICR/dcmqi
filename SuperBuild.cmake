QIICR/dcmqi

commit 9dd0c6cc164c48f061881fda0847241a062012de

BUG: cmake: Ensure git and svn executable are propagated
This problem was not revealed when Slicer extensions are
configured and built automatically by the Extensions build system setup
on the factory machines. In that case, variables like git and svn paths
are explicitly passed to the extension and added to the cache
using the "-D" cmake argument.

Instead, this commit fixes a problem occurring only when:

(1) svn or git is NOT in  the path

and

(2) extension is configured by only passing
    "-DSlicer_DIR:PATH=/path/to/Slicer-SuperBuild-Release/Slicer-build/"

In that case, because the values of Subversion_SVN_EXECUTABLE and
GIT_EXECUTABLE are retrieved in ExternalProjectDependency.cmake
in way that trigger https://gitlab.kitware.com/cmake/cmake/issues/15448

This commit explicitly pass then in SuperBuild.cmake despite the fact
they are already "mark_as_superbuild" in SlicerEnableExtensionTesting.cmake
used within SlicerConfig.cmake

SuperBuild.cmake
@@ -68,6 +68,10 @@ ExternalProject_Add(${proj}
     -DCMAKE_CXX_FLAGS_INIT:STRING=${CMAKE_CXX_FLAGS_INIT}
     -DCMAKE_C_FLAGS_INIT:STRING=${CMAKE_C_FLAGS_INIT}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+    # XXX Workaround https://gitlab.kitware.com/cmake/cmake/issues/15448
+    #     and explicitly pass GIT_EXECUTABLE and Subversion_SVN_EXECUTABLE
+    -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
+    -DSubversion_SVN_EXECUTABLE:FILEPATH=${Subversion_SVN_EXECUTABLE}
   SOURCE_DIR ${DCMQI_SOURCE_DIR}
   BINARY_DIR ${DCMQI_BINARY_DIR}/${PROJECT_NAME_LC}-build
   PREFIX ${PROJECT_NAME_LC}-prefix

commit b3ea5fa05f204d2575e2c898b614159a9a56bda7

BUG: cmake/SuperBuild: Only propagate svn and git paths if valid values
This commit fixes windows build addressing a regression introduced
in 9dd0c6c (BUG: cmake: Ensure git and svn executable are propagated)

SuperBuild.cmake
@@ -56,6 +56,15 @@ if(NOT DCMQI_BUILD_SLICER_EXTENSION)
     )
 endif()
 
+set(DCMQI_EP_CMAKE_CACHE_ARGS)
+# XXX Workaround https://gitlab.kitware.com/cmake/cmake/issues/15448
+#     and explicitly pass GIT_EXECUTABLE and Subversion_SVN_EXECUTABLE
+foreach(varname IN ITEMS GIT_EXECUTABLE Subversion_SVN_EXECUTABLE)
+  if(EXISTS "${${varname}}")
+    list(APPEND DCMQI_EP_CMAKE_CACHE_ARGS -D${varname}:FILEPATH=${${varname}})
+  endif()
+endforeach()
+
 ExternalProject_Add(${proj}
   ${${proj}_EP_ARGS}
   DOWNLOAD_COMMAND ""
@@ -68,10 +77,6 @@ ExternalProject_Add(${proj}
     -DCMAKE_CXX_FLAGS_INIT:STRING=${CMAKE_CXX_FLAGS_INIT}
     -DCMAKE_C_FLAGS_INIT:STRING=${CMAKE_C_FLAGS_INIT}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    # XXX Workaround https://gitlab.kitware.com/cmake/cmake/issues/15448
-    #     and explicitly pass GIT_EXECUTABLE and Subversion_SVN_EXECUTABLE
-    -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
-    -DSubversion_SVN_EXECUTABLE:FILEPATH=${Subversion_SVN_EXECUTABLE}
   SOURCE_DIR ${DCMQI_SOURCE_DIR}
   BINARY_DIR ${DCMQI_BINARY_DIR}/${PROJECT_NAME_LC}-build
   PREFIX ${PROJECT_NAME_LC}-prefix