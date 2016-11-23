bfbd79b5067bd5e8c8453b5f80563ebe4484923b
Andrey Fedorov <fedorov@bwh.harvard.edu>
2016-11-23 14:02:17 -0500

    ENH: add error reporting and exit more gracefully

--- libsrc/ImageSEGConverter.cpp
+++ libsrc/ImageSEGConverter.cpp
@@ -382,7 +382,10 @@ namespace dcmqi {
       CHECK_COND(segdoc->getFrameOfReference().setFrameOfReferenceUID(frameOfRefUIDchar));
     }
 
-    CHECK_COND(segdoc->writeDataset(segdocDataset));
+    if(segdoc->writeDataset(segdocDataset).bad()){
+      cerr << "FATAL ERROR: Writing of the SEG dataset failed! Please report the problem to the developers, ideally accompanied by a de-identified dataset allowing to reproduce the problem!" << endl;
+      return NULL;
+    }
 
     // Set reader/session/timepoint information
     CHECK_COND(segdocDataset.putAndInsertString(DCM_SeriesDescription, metaInfo.getSeriesDescription().c_str()));
