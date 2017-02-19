cda0be54ce13291f4b64ecc84de626c2f3dbbbbd
Andrey Fedorov <fedorov@bwh.harvard.edu>
2017-02-19 16:42:00 -0500

    ENH: clean up docker_entry script

--- docker_entry.sh
+++ docker_entry.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-if [[ $1 = 'itkimage2paramap' ]]; then
-   /usr/src/dcmqi-superbuild/dcmqi-build/bin/itkimage2paramap "${@:2}"
-elif [[ $1 = 'paramap2itkimage' ]]; then
-   /usr/src/dcmqi-superbuild/dcmqi-build/bin/paramap2itkimage "${@:2}"
-elif [[ $1 = 'itkimage2segimage' ]]; then
-  /usr/src/dcmqi-superbuild/dcmqi-build/bin/itkimage2segimage "${@:2}"
-elif [[ $1 = 'segimage2itkimage' ]]; then
-  /usr/src/dcmqi-superbuild/dcmqi-build/bin/segimage2itkimage "${@:2}"
-elif [[ $1 = 'tid1500reader' ]]; then
-   /usr/src/dcmqi-superbuild/dcmqi-build/bin/tid1500reader "${@:2}"
-elif [[ $1 = 'tid1500writer' ]]; then
-   /usr/src/dcmqi-superbuild/dcmqi-build/bin/tid1500writer "${@:2}"
+BIN_PATH=/usr/src/dcmqi-superbuild/dcmqi-build/bin
+
+CLIs=`cd $BIN_PATH;find . -type f -perm 755 | cut -d'/' -f 2 | tr '\n' ' '`
+
+requestedCLI=${BIN_PATH}/${1}
+
+if [ -x $requestedCLI]; then
+  $requestedCLI ${@:2}
 else
-  echo "Command not recognized!" \
-    "Allowed commands: itkimage2paramap, " \
-    "paramap2itkimage, itkimage2segimage, " \
-    "segimage2itkimage, tid1500reader, " \
-    "tid1500writer"
+  echo "Command not recognized! Recognized dcmqi command line converters: ${CLIs}"
 fi
