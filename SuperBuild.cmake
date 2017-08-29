QIICR/dcmqi

commit b5691998ccd75e12dcdf700c2b81f33a29d8a695

cmake: Introduce PROJECT_NAME_LC var to allow consistent naming of inner build dir

SuperBuild.cmake
@@ -34,10 +34,10 @@ set(DCMQI_DEPENDENCIES
 #-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
-# Make sure ${DCMQI_BINARY_DIR}/DCMQI-build/bin exists
+# Make sure ${DCMQI_BINARY_DIR}/${PROJECT_NAME_LC}-build/bin exists
 # May be used by some external project to install libs
-if(NOT EXISTS ${DCMQI_BINARY_DIR}/dcmqi-build/bin)
-  file(MAKE_DIRECTORY ${DCMQI_BINARY_DIR}/dcmqi-build/bin)
+if(NOT EXISTS ${DCMQI_BINARY_DIR}/${PROJECT_NAME_LC}-build/bin)
+  file(MAKE_DIRECTORY ${DCMQI_BINARY_DIR}/${PROJECT_NAME_LC}-build/bin)
 endif()
 
 #-----------------------------------------------------------------------------
@@ -59,7 +59,8 @@ ExternalProject_Add(${proj}
     -DCMAKE_C_FLAGS_INIT:STRING=${CMAKE_C_FLAGS_INIT}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
   SOURCE_DIR ${DCMQI_SOURCE_DIR}
-  BINARY_DIR ${DCMQI_BINARY_DIR}/dcmqi-build
+  BINARY_DIR ${DCMQI_BINARY_DIR}/${PROJECT_NAME_LC}-build
+  PREFIX ${PROJECT_NAME_LC}-prefix
   INSTALL_COMMAND ""
   DEPENDS
     ${DCMQI_DEPENDENCIES}

CMakeLists.txt
@@ -29,6 +29,7 @@ if(APPLE)
 endif()
 
 project(DCMQI)
+string(TOLOWER ${PROJECT_NAME} PROJECT_NAME_LC)
 
 #-----------------------------------------------------------------------------
 # Library mode: SHARED (default) or STATIC
@@ -90,7 +91,7 @@ foreach(type LIBRARY RUNTIME ARCHIVE)
   if(DCMQI_SUPERBUILD)
     set(output_dir ${DCMQI_BINARY_DIR}/bin)
     if(NOT DEFINED DCMQI_CMAKE_${type}_OUTPUT_DIRECTORY)
-      set(DCMQI_CMAKE_${type}_OUTPUT_DIRECTORY ${DCMQI_BINARY_DIR}/dcmqi-build/bin)
+      set(DCMQI_CMAKE_${type}_OUTPUT_DIRECTORY ${DCMQI_BINARY_DIR}/${PROJECT_NAME_LC}-build/bin)
     endif()
     mark_as_superbuild(DCMQI_CMAKE_${type}_OUTPUT_DIRECTORY:PATH)
   else()

commit f67a557e797af197b71604753bf59aafd0d3395a

cmake: Fix forcebuild step
This commit ensures the step attempts to remove an existing file by
fixing a regression introduced in b569199 (cmake: Introduce PROJECT_NAME_LC
var to allow consistent naming of inner build dir)

SuperBuild.cmake
@@ -100,9 +100,9 @@ ExternalProject_Add(${proj}
 # version will be at least 3.1.
 #
 if(CMAKE_CONFIGURATION_TYPES)
-  set(BUILD_STAMP_FILE "${CMAKE_CURRENT_BINARY_DIR}/${proj}-prefix/src/${proj}-stamp/${CMAKE_CFG_INTDIR}/${proj}-build")
+  set(BUILD_STAMP_FILE "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME_LC}-prefix/src/${proj}-stamp/${CMAKE_CFG_INTDIR}/${proj}-build")
 else()
-  set(BUILD_STAMP_FILE "${CMAKE_CURRENT_BINARY_DIR}/${proj}-prefix/src/${proj}-stamp/${proj}-build")
+  set(BUILD_STAMP_FILE "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME_LC}-prefix/src/${proj}-stamp/${proj}-build")
 endif()
 ExternalProject_Add_Step(${proj} forcebuild
   COMMAND ${CMAKE_COMMAND} -E remove ${BUILD_STAMP_FILE}